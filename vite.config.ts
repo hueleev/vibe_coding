a5844002caa402363300d5ed108f3eff3237fb60
leehyungwon <hueleev@dkitec.com>
2025-07-01 17:00:25 +0900

    update scriptURL

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
       // '/api'로 시작하는 요청을 구글 앱스 스크립트로 프록시
       '/api': {
         target:
-          'https://script.google.com/macros/s/AKfycbyuA1HkijwAef48rj6cfoClA7e-G4d3jYlj6lA3KympQmo9PDWA2bweOe7tHdjtwIIIkw/exec',
+          'https://script.google.com/macros/s/AKfycbwA__xHYwalpuAEk62QZfxJOXnSUiE9whY4nHb_RMamcPgn7G1LsW5JdIKzDgmFdfJmxQ/exec',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false,
